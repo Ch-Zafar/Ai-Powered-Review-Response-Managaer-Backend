69571eafedb8a0478a892d3236e507ba41842d69
Ch-Zafar <111778881+Ch-Zafar@users.noreply.github.com>
2026-06-27 23:53:54 +0500

    multer completed

--- controller/upload.controller.js
+++ controller/upload.controller.js
@@ -1,22 +1,37 @@
-const express = require('express')
-const multer  = require('multer')
-const fs  = require('fs')
+const fs = require("fs");
+const csv = require("csv-parser");
 
+const uploadController = (req, res) => {
 
-const uploadController = (req,res)=>{
-    const results = []
+    if (!req.file) {
+        return res.status(400).json({
+            message: "No file uploaded"
+        });
+    }
+
+    const results = [];
+
     fs.createReadStream(req.file.path)
-    .pipe(csv())
-    .on('data',(data)=>{
-        results.push(data)
-    })
-    .on('end',()=>{
-        console.log(results)
-        res.json({
-            message:"CSV Uploaded Succesfully",
-            data:results
-           })
-    })
-}
+        .pipe(csv())
+        .on("data", (data) => {
+            results.push(data);
+        })
+        .on("end", () => {
+            console.log(results);
 
-module.exports = {uploadController}
+            res.json({
+                message: "CSV Uploaded Successfully",
+                data: results
+            });
+        })
+        .on("error", (error) => {
+            console.log(error);
+
+            res.status(500).json({
+                message: "Error reading CSV file",
+                error
+            });
+        });
+};
+
+module.exports = { uploadController };
